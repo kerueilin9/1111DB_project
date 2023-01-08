cfd9a19d2d48949e1fd06e0cb8b5806483206e25
ZhuangZheAn <97737497+ZhuangZheAn@users.noreply.github.com>
2023-01-08 15:46:51 +0800

    Update app.py

--- Back_end/app.py
+++ Back_end/app.py
@@ -1,9 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flaskext.mysql import MySQL
 import pymysql
-
-
+import json
 # configuration
 DEBUG = True
  
@@ -18,19 +18,36 @@ app.config['MYSQL_DATABASE_PASSWORD'] = 'admin'
 app.config['MYSQL_DATABASE_DB'] = 'shopDB'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
- 
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
- 
-@app.route('/')
+
+@app.route('/signUp', methods=['POST'])
+def signUp():
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    try:
+        data = request.get_json()
+        sql = ("INSERT INTO user (Name, Email, Account, Password, Role, Phone, Gender) VALUES (%s, %s, %s, %s, %s, %s, %s)")
+        val = (data['Name'], data['Email'], data['Account'], data['Password'], 'member', data['Phone'], data['Gender'])
+        cursor.execute(sql, val)
+        conn.commit()
+        return jsonify({
+            'status': 'success'
+        })
+    finally:
+        cursor.close() 
+        conn.close()
+
+@app.route('/userList', methods=['GET'])
 def home():
     conn = mysql.connect()
     cursor = conn.cursor(pymysql.cursors.DictCursor)
+
     try:
         cursor.execute("SELECT * from user")
         userslist = cursor.fetchall()
         return jsonify({
-            'status': 'success',    
+            'status': 'success',
             'members': userslist
         })
     except Exception as e:
@@ -38,5 +55,6 @@ def home():
     finally:
         cursor.close() 
         conn.close()
+
 if __name__ == '__main__':
     app.run()
