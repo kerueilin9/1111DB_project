84bb3e4305125f7f4d2a44ad0fd4765d40cf44e2
yuanhao <t109590041@ntut.org.tw>
2023-01-08 22:37:44 +0800

    [database] 修改 product  `Discount` 型別為 int

--- Database/shopdb_product.sql
+++ Database/shopdb_product.sql
@@ -30,7 +30,7 @@ CREATE TABLE `product` (
   `Describe` varchar(256) NOT NULL,
   `Quantity` int NOT NULL,
   `Price` int NOT NULL,
-  `Discount` decimal(3,2) NOT NULL,
+  `Discount` int NOT NULL,
   `Discount_period` datetime DEFAULT NULL,
   PRIMARY KEY (`PID`),
   KEY `Manager_ID` (`Manager_ID`),
@@ -45,8 +45,8 @@ CREATE TABLE `product` (
 LOCK TABLES `product` WRITE;
 /*!40000 ALTER TABLE `product` DISABLE KEYS */;
 INSERT INTO `product` VALUES 
-(1,_binary 'image',1,'block','This is a square',5,1200,1.00,'2023-12-12 12:59:23'), 
-(2,_binary 'image',1,'red','This is a book',9,300,100.00,'0000-00-00 00:00:00');
+(1,_binary 'image',1,'block','This is a square',5,1200,1,'2023-12-12 12:59:23'), 
+(2,_binary 'image',1,'red','This is a book',9,300,100,'0000-00-00 00:00:00');
 /*!40000 ALTER TABLE `product` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
